58311cc0058dea334777282b6381aceb72952293
Pedro4000 <P.brickley@hotmail.fr>
2022-09-01 15:04:21 +0200

    ajout docker

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-var Encore = require('@symfony/webpack-encore');
+const Encore = require('@symfony/webpack-encore');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -17,37 +17,21 @@ Encore
     /*
      * ENTRY CONFIG
      *
-     * Add 1 entry for each "page" of your app
-     * (including one that's included on every page - e.g. "app")
-     *
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-     .copyFiles({
-         from: './assets/img',
+    .addEntry('app', './assets/app.js')
 
-     })
-    .addEntry('signup', './assets/js/signup.js')
-    .addEntry('index', './assets/js/discogs.js')
-    .addEntry('app', './assets/js/app.js')
-    .addEntry('bootstrapjs', './assets/bootstrap-4.5.3-dist/js/bootstrap.js')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
+    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
+    .enableStimulusBridge('./assets/controllers.json')
+
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
     // will require an extra script tag for runtime.js
-
     // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
-    .enableLessLoader()
 
-    .cleanupOutputBeforeBuild()
-/*    .copyFiles({
-        from: './assets/img',
-        to: '[path][name].[hash:8].[ext]',
-        context: './assets'
-    })*/
     /*
      * FEATURE CONFIG
      *
@@ -61,6 +45,10 @@ Encore
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
 
+    .configureBabel((config) => {
+        config.plugins.push('@babel/plugin-proposal-class-properties');
+    })
+
     // enables @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
@@ -68,25 +56,20 @@ Encore
     })
 
     // enables Sass/SCSS support
-    .enableSassLoader()
+    //.enableSassLoader()
 
-// uncomment if you use TypeScript
-//.enableTypeScriptLoader()
+    // uncomment if you use TypeScript
+    //.enableTypeScriptLoader()
 
-// uncomment to get integrity="..." attributes on your script & link tags
-// requires WebpackEncoreBundle 1.4 or higher
-//.enableIntegrityHashes(Encore.isProduction())
+    // uncomment if you use React
+    //.enableReactPreset()
 
-// uncomment if you're having problems with a jQuery plugin
-//.autoProvidejQuery()
+    // uncomment to get integrity="..." attributes on your script & link tags
+    // requires WebpackEncoreBundle 1.4 or higher
+    //.enableIntegrityHashes(Encore.isProduction())
 
-// uncomment if you use API Platform Admin (composer req api-admin)
-//.enableReactPreset()
-//.addEntry('admin', './assets/js/admin.js')
+    // uncomment if you're having problems with a jQuery plugin
+    //.autoProvidejQuery()
 ;
-if (!Encore.isRuntimeEnvironmentConfigured()) {
-    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
-
-}
 
 module.exports = Encore.getWebpackConfig();
